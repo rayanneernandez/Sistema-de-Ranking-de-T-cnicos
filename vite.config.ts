17cd1bf4f4630e3d2aa1316a6beb796e970e7ac9
Rayanne Ernandez <168318292+rayanneernandez@users.noreply.github.com>
2025-05-03 22:07:39 -0300

    analise

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sistema-de-ranking-tecnicos/', // Define o caminho base para GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
